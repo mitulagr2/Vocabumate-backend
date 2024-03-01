27c4ed99cee78a6f37c6d205cb2cbd94e0c69535
mitulagr2 <mitulagr2@gmail.com>
2024-03-01 17:04:28 +0530

    setup prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 import generateContent from "./generate";
 
